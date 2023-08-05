4b4094fe4d826b96b4b7235d861d3e7e3e762243
Nimisha Sinha <119108244+NimishaSinha03@users.noreply.github.com>
2023-08-05 17:52:46 +0530

    Time: 207 ms (8.06%), Space: 25.7 MB (9.05%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        vector<int>temp;
+        temp=nums;
+        for(int i=0;i<nums.size();i++){
+            temp.push_back(nums[i]);
+        }
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+           int x = temp[i];
+            int j = i + 1;
+            while(j < temp.size()){
+                if(temp[j] > x){
+                    x = temp[j];
+                    break;
+                }
+                j++;
+            }
+            if(x != temp[i]) ans.push_back(x);
+            else ans.push_back(-1);
+        }
+        return ans;
+    }
+};
+// dry run kr aa jaayega
