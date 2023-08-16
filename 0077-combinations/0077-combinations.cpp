152896accf712e827b01a6bb3733c963292e6f9c
Nimisha Sinha <119108244+NimishaSinha03@users.noreply.github.com>
2023-08-16 16:04:53 +0530

    Time: 137 ms (15.10%), Space: 111.1 MB (13.76%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void solve(int i, int n,int k, vector<vector<int>>&ans, vector<int>&res){
+        if(k==0){ans.push_back(res);return;}
+        if(i>=n+1)return;
+        solve(i+1,n,k,ans, res);
+        res.push_back(i);
+        solve(i+1,n,k-1,ans, res);
+        res.pop_back();
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>>ans;
+        vector<int>res;
+        solve(1,n,k,ans,res);
+        return ans;
+    }
+};
