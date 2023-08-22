f5ead7518555abb29f1c765e8206efd3b71499c1
Nimisha Sinha <119108244+NimishaSinha03@users.noreply.github.com>
2023-08-22 23:39:39 +0530

    Time: 287 ms (25.13%), Space: 49.4 MB (15.85%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,27 +2,25 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         set<vector<int>>st;
-        vector<vector<int>>ans;
         int n=nums.size();
         sort(nums.begin(),nums.end());
-      for(int i=0;i<n-3;i++){
-          for(int j=i+1;j<n-2;j++){
-              long long neww = (long long)target - (long long)nums[i] - (long long)nums[j];
-              int l=j+1,h=n-1;
-              while(l<h){
-                  if(nums[l]+nums[h]<neww)l++;
-                  else if(nums[l]+nums[h]>neww)h--;
-                  else 
-                  {st.insert({nums[i],nums[j],nums[l],nums[h]});
-                  l++;
-                  h--;
-                  }
-              }
-          }
-      }
-      for(auto it:st){
-          ans.push_back(it);
-      }  
-      return ans;
+        for(int i=0;i<n-3;i++){
+            for(int j=i+1;j<n-2;j++){
+               long long neww = (long long)target - (long long)nums[i] - (long long)nums[j];
+                int l=j+1,h=n-1;
+                while(l<h){
+                    if(nums[l]+nums[h]<neww)l++;
+                    else if(nums[l]+nums[h]>neww)h--;
+                    else 
+                        {st.insert({nums[i],nums[j],nums[l],nums[h]});
+                         l++;
+                         h--;
+                         }
+                }
+            }
+        }
+        vector<vector<int>>ans;
+        for(auto x:st){ans.push_back(x);}
+        return ans;
     }
 };
