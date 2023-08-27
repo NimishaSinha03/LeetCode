b6364d5c47e311f2eebca4e215cd9a2719a976c1
Nimisha Sinha <119108244+NimishaSinha03@users.noreply.github.com>
2023-08-27 23:35:21 +0530

    Time: 9 ms (45.35%), Space: 7.7 MB (49.86%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head==NULL||head->next==NULL)return NULL;
+        ListNode *slow=head, *fast=head;
+        while(fast){
+            fast=fast->next;
+            slow=slow->next;
+            if(fast)fast=fast->next;
+            // same as detecting cycle, ab set fast to head, and move slow and fast ointers by 1(same pace), and return fast
+            if(slow==fast){fast=head;
+            while(fast!=slow){
+                fast=fast->next;
+                slow=slow->next;
+            }
+            return fast;
+        }}
+        return NULL;
+    }
+};
