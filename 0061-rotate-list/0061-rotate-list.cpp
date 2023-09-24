51046906e85999f27c3124811dffac52d1460d89
Nimisha Sinha <119108244+NimishaSinha03@users.noreply.github.com>
2023-09-25 01:41:00 +0530

    Time: 4 ms (69.72%), Space: 12 MB (14.51%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -11,24 +11,24 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-         if (!head || k == 0) {
-            return head;
-        }
-        ListNode* c=head;
-       
-        ListNode* ans=NULL;
+        if(!head||k==0)return head;
+        // to find no. of nodes
+        ListNode* counter=head;
         int count=1;
-        while(c->next){
+        while(counter->next){
             count++;
-            c=c->next;
+            counter=counter->next;
         }
-       k = k % count;
+        k=k%count;
         if(k==0)return head;
-        c->next=head;
-        c=head;
-        for(int i=0;i<count-k-1;i++)c=c->next;
-        ans=c->next;
-        c->next=NULL;
+        counter->next=head;
+        counter=head;
+        for(int i=0;i<count-k-1;i++){
+            counter=counter->next;
+        }
+        ListNode* ans=NULL;
+        ans=counter->next;
+        counter->next=NULL;
         return ans;
     }
 };
