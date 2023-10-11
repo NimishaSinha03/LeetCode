5bd24a177455b0787f06889d0c784575ac2b89b3
Nimisha Sinha <119108244+NimishaSinha03@users.noreply.github.com>
2023-10-12 00:24:53 +0530

    Time: 8 ms (18.92%), Space: 11.3 MB (26.81%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -6,11 +6,18 @@ public:
         while(s<=e){
             int mid=(s+e)/2;
             if(nums[mid]==target){return mid;}
-            if(nums[s]<=nums[mid]){
-                if(nums[s]<=target&&target<nums[mid]){e=mid-1;}
+            if(nums[s]<=nums[mid])
+                // means left part iis sorted
+            {
+                if(nums[s]<=target&&target<nums[mid])
+                // terget lies within the left sorted part
+                {e=mid-1;}
+                // else target lies in right part
                 else {s=mid+1;  }
             }
+            // if left part is not sorted
             else{
+                // therfore checking in the right part, since right val atoh sorted hoga hi-->think
                 if(nums[mid]<target&&target<=nums[e]){s=mid+1;}
                 else {e=mid-1;}
             }
