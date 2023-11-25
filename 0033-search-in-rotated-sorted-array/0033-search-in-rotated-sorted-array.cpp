bb580d48804fb0c6728ba7fde042362f2771e996
Nimisha Sinha <119108244+NimishaSinha03@users.noreply.github.com>
2023-11-25 23:09:48 +0530

    Time: 3 ms (64.84%), Space: 11.5 MB (15.30%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n=nums.size();
-        int s=0,e=n-1;
+        int s=0, e=nums.size()-1;
         while(s<=e){
-            int mid=(s+e)/2;
-            if(nums[mid]==target){return mid;}
-            if(nums[s]<=nums[mid])
-                // means left part iis sorted
-            {
-                if(nums[s]<=target&&target<nums[mid])
-                // terget lies within the left sorted part
-                {e=mid-1;}
-                // else target lies in right part
-                else {s=mid+1;  }
+            int mid = (s + e) / 2;
+            if(nums[mid]==target)return mid;
+            // dekh ab ya toh iss line m lie kareg ayaa uss line m,
+            // toh dono conditions to if ele se m daal
+            if( nums[s]<=nums[mid]){
+                if(nums[s]<=target&&nums[mid]>target)e=mid-1;
+                else s=mid+1;
             }
-            // if left part is not sorted
             else{
-                // therfore checking in the right part, since right val atoh sorted hoga hi-->think
-                if(nums[mid]<target&&target<=nums[e]){s=mid+1;}
-                else {e=mid-1;}
+                if(target>nums[mid]&&target<=nums[e])s=mid+1;
+                else e=mid-1;
             }
-            
+            // return -1;
         }
         return -1;
     }
