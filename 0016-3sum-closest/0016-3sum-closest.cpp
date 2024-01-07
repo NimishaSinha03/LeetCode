52d81b7ce6ff4ca88d4062babe5c932b62bc9255
Nimisha Sinha <119108244+NimishaSinha03@users.noreply.github.com>
2024-01-07 22:52:19 +0530

    Time: 38 ms (48.76%), Space: 10.6 MB (53.54%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -3,43 +3,23 @@ public:
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
         int s=nums[0]+nums[1]+nums[2];
-        for(int i=0;i<nums.size()-1;i++){
+        for(int i=0;i<nums.size()-2;i++){
             int j=i+1,k=nums.size()-1;
+            // int sum=nums[i]+nums[j]+nums[k];
             while(j<k){
                 int sum=nums[i]+nums[j]+nums[k];
-                // int diff=abs(target-sum);
                 if(abs(sum-target)<abs(s-target))s=sum;
                 if(sum<target)j++;
                 else k--;
-                
             }
         }
         return s;
     }
 };
-// class Solution {
-// public:
-//     int threeSumClosest(vector<int>& nums, int target) {
-//         sort(nums.begin(),nums.end());
-//         int nearestsum=nums[0]+nums[1]+nums[2];
-//         for(int i=0;i<nums.size()-1;i++){
-         
-//             int start=i+1;
-//             int end=nums.size()-1;
-
-//             while(start<end){
-//                 int currentsum =nums[start]+nums[end]+nums[i];
-//                  if(abs(currentsum-target)<abs(nearestsum-target)){
-//                         nearestsum=currentsum;
-//                     } 
-//                 if(currentsum<target){
-//                     start++;
-//                 } else {
-//                     end--;
-//                 }
-//             }
-          
-//         }
-//         return nearestsum;
-//     }
-// };
+// logic
+// isme basically 3 pointers liye hai
+// i, j, k;
+// ek baar i remains constant, and all the values of j and k are checked inside the for loop
+// j is the pointer of i, and k is the laset one , so yaha pe we are finding out, that for one value of i, konsa j and k are perfect (using while loop)
+//     and the for next i , j and k are found out, and if it is better than the previous triplet, the sum is updated accodingly using (                if(abs(sum-target)<abs(s-target))s=sum;
+// )
