31d0b6338b0abc8f935f4068c00768983206c37e
Nimisha Sinha <119108244+NimishaSinha03@users.noreply.github.com>
2024-01-19 14:45:26 +0530

    Time: 5 ms (64.34%), Space: 7.6 MB (86.66%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
-    void solve(vector<vector<string>>&ans, vector<string>& board, int n,int col, vector<int>&side, vector<int>&upperd, vector<int>&lowerd){
+    void solve(vector<vector<string>>&ans, int n, int col,vector<string>&board, vector<int>&side, vector<int>&upperd,vector<int>&lowerd){
         if(col==n){
             ans.push_back(board);
             return;
         }
         for(int row=0;row<n;row++){
-            if(side[row]==0&&lowerd[col+row]==0&&upperd[n-1+col-row]==0){
+            if(side[row]==0&&upperd[n-1+col-row]==0&&lowerd[row+col]==0){
                 board[row][col]='Q';
                 side[row]=1;
-                lowerd[col+row]=1;
                 upperd[n-1+col-row]=1;
-                solve(ans, board, n, col+1,side, upperd, lowerd);
+                lowerd[row+col]=1;
+                solve(ans,n,col+1,board,side,upperd, lowerd);
                 board[row][col]='.';
                 side[row]=0;
-                lowerd[col+row]=0;
                 upperd[n-1+col-row]=0;
+                lowerd[row+col]=0;
             }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>>ans;
         vector<string>board(n);
-        string s(n,'.');
+         string s(n,'.');
         for(int i=0;i<n;i++){
             board[i]=s;
         }
-        vector<int>side(n,0),upperd(2*n-1,0),lowerd(2*n-1,0);
-        solve(ans, board, n, 0, side, upperd, lowerd);
+        vector<int>side(n,0);
+        vector<int>upperd(2*n-1,0);
+        vector<int>lowerd(2*n-1,0);
+        solve(ans,n,0,board,side,upperd,lowerd);
         return ans;
     }
 };
