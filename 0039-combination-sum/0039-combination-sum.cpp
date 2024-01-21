94623b941f4f13aa8ec626b54b61e3bd4b8f0cdc
Nimisha Sinha <119108244+NimishaSinha03@users.noreply.github.com>
2024-01-22 00:02:07 +0530

    Time: 3 ms (86.46%), Space: 16.4 MB (33.92%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    void solve(vector<int>&candidates, int target, int i, vector<int>output){
-        if(i>=candidates.size()||target<0)return;
-        if(target==0){ans.push_back(output);return;}
+        vector<vector<int>>ans;
+
+    void solve(vector<int>&candidates,int target, int i,vector<int>&output){
+        if(i>=candidates.size()||target<0)return ;
+        if(target==0){
+            ans.push_back(output);
+            return;
+        }
         solve(candidates,target,i+1,output);
         output.push_back(candidates[i]);
         solve(candidates,target-candidates[i],i,output);
         output.pop_back();
+        
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        // ans.clear();
+        // vector<vector<int>>ans;
         vector<int>output;
         solve(candidates,target,0,output);
         return ans;
