c40b33a0c14cbaa2645c8725b590ca7b241b11c6
Nimisha Sinha <119108244+NimishaSinha03@users.noreply.github.com>
2024-01-21 23:47:29 +0530

    Time: 71 ms (16.01%), Space: 69.8 MB (11.92%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -10,7 +10,7 @@ public:
         output.pop_back();
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        ans.clear();
+        // ans.clear();
         vector<int>output;
         solve(candidates,target,0,output);
         return ans;
