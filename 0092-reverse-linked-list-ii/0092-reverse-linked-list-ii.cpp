5492f10152d09e03aa76848c781899a2753e62a2
Nimisha Sinha <119108244+NimishaSinha03@users.noreply.github.com>
2024-01-23 00:01:42 +0530

    Time: 5 ms (6.36%), Space: 9.5 MB (15.62%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -8,53 +8,45 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
 class Solution {
 public:
-    ListNode* reverseBetween(ListNode* head, int left, int right){
-        if(head==NULL) return head;
-        if(head->next==NULL) return head;
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        if(head==NULL)return head;
+        if(head->next==NULL)return head;
         int l=left;
-        ListNode *prev=NULL;
-        ListNode* ans=head;
-       
-
-        while(l>1)
-        {
+        ListNode * prev=NULL;
+        ListNode * ans=head;
+        while(l>1){
             prev=head;
             head=head->next;
             l--;
         }
-        int loop=right-left+1;
-        ListNode* pre=NULL;
-        ListNode* nex;
-        ListNode* last;
+        // now prev is at previous of left
+        int loop= right-left+1;
+        ListNode * pre=NULL;
+        ListNode * nex;
         ListNode* prev2=head;
-
-
-        while(loop)
-        {
-            if(head)
-           { 
-               nex=head->next;
+        while(loop){
+            if(head){
+            nex=head->next;
             head->next=pre;
             pre=head;
             head=nex;
             }
-
             loop--;
         }
-        if(prev!=nullptr)
-        {if(pre!=nullptr)
-        {
-            prev->next=pre;
+        // now pre is the head of reversed part of the linked list
+        if(prev!=NULL){
+            if(pre!=NULL)
+            {
+                // prev->next->next = head;
+                prev->next=pre;
+            }
+            else prev->next=NULL;
         }
-
-
-        else prev->next=NULL;}
+        
         else ans=pre;
         prev2->next=head;
-
         return ans;
     }
 };
