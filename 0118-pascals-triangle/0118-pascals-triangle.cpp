64371a98259ba7fc8f5d23e1bdca36ba10435da4
Nimisha Sinha <119108244+NimishaSinha03@users.noreply.github.com>
2024-03-06 14:59:28 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (73.79%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -2,29 +2,15 @@ class Solution {
 public:
     vector<vector<int>> generate(int n) {
         vector<vector<int>>ans;
-        // ans.push_back((1));
         for(int i=0;i<n;i++){
             ans.push_back(vector<int>(i+1,1));
         }
         for(int i=2;i<n;i++){
-            for(int j=1;j<ans[i].size()-1;j++)
+            for(int j=1;j<ans[i].size()-1;j++){
                 ans[i][j]=ans[i-1][j-1]+ans[i-1][j];
-    }
+            }
+        }
         return ans;
+        
     }
 };
-// class Solution {
-//  public:
-//   vector<vector<int>> generate(int numRows) {
-//     vector<vector<int>> ans;
-
-//     for (int i = 0; i < numRows; ++i)
-//       ans.push_back(vector<int>(i + 1, 1));
-
-//     for (int i = 2; i < numRows; ++i)
-//       for (int j = 1; j < ans[i].size() - 1; ++j)
-//         ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j];
-
-//     return ans;
-//   }
-// };
