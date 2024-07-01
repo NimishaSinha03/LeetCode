64f10ba2878e4ca744a02a5c01a4277d60d2cc7c
Nimisha Sinha <119108244+NimishaSinha03@users.noreply.github.com>
2024-07-01 23:18:14 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (52.23%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        int total=n*m;
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int total= m*n;
         int count=0;
-        vector<int>v;
-        int sr=0;
-        int sc=0;
-        int er=n-1;
-        int ec=m-1;
+        int sc=0,sr=0,ec=n-1,er=m-1;
+        vector<int>ans;
         while(count<total)
         {for(int i=sc;i<=ec&&count<total;i++){
-            v.push_back(matrix[sr][i]);
+            ans.push_back(matrix[sr][i]);
             count++;
         }
         sr++;
         for(int i=sr;i<=er&&count<total;i++){
-            v.push_back(matrix[i][ec]);
+            ans.push_back(matrix[i][ec]);
             count++;
         }
         ec--;
         for(int i=ec;i>=sc&&count<total;i--){
-            v.push_back(matrix[er][i]);
+            ans.push_back(matrix[er][i]);
             count++;
         }
         er--;
         for(int i=er;i>=sr&&count<total;i--){
-            v.push_back(matrix[i][sc]);
+            ans.push_back(matrix[i][sc]);
             count++;
         }
         sc++;}
-        return v;
-        
+        return ans;
     }
 };
