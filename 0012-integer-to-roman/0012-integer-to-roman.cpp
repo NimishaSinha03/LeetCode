a5a219192a3cc9737e44067b1d54de65cb341e2a
Nimisha Sinha <119108244+NimishaSinha03@users.noreply.github.com>
2024-07-03 13:24:29 +0530

    Time: 13 ms (17.13%), Space: 12.7 MB (34.90%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    string intToRoman(int num) {
-        string ans ="";
-        vector<int>numb={1,4,5,9,10,40,50,90,100,400,500,900,1000};
-        vector<string>rom={"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"
-                };
+    string intToRoman(int n) {
+        string ans="";
+        vector<int>num={1,4,5,9,10,40,50,90,100,400,500,900,1000};
+        vector<string>rom={"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
         for(int i=12;i>=0;i--){
-            while(num>=numb[i]){
+            while(n>=num[i]){
                 ans+=rom[i];
-                num-=numb[i];
+                n-=num[i];
             }
         }
         return ans;
